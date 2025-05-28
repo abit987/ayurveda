da7b0134f4319e1e1e39fab38b17ef3f62a13ca3
abit987 <kurichheashan@gmail.com>
2025-05-28 09:53:36 +0530

    Updates

--- src/pages/admin/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-function Dashboard() {
-    return (
-        <div className="text-center py-5">
-            <h1>Welcome Admin</h1>
-        </div>
-    )
-}
-
-export default Dashboard;
--- src/pages/users/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-function Dashboard() {
-    return (
-        <div className="text-center">
-            <h1>Welcome User</h1>
-        </div>
-    )
-}
-
-export default Dashboard;
